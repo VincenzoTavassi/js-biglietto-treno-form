d86fb48ffd79d7a4a3037df5d41a135597738535
VincenzoTavassi <info@egadivacanze.it>
2023-01-24 18:18:02 +0100

    funzionante con grafica

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ document.getElementById('calcola').addEventListener('click',
         const chilometri = parseInt(document.querySelector('#km').value);
         const etaPasseggero = document.querySelector('#eta-utente').value;
         const nomeUtente = document.querySelector('#nome-utente').value;
-        console.log(etaPasseggero);
         // CONTROLLO CHE IL CAMPO NOME NON SIA VUOTO
         if (nomeUtente.length < 5) {
             document.getElementById('risultato').innerHTML = `E' necessario inserire un nome valido.`;
@@ -24,20 +23,30 @@ document.getElementById('calcola').addEventListener('click',
             const tariffa = 0.21;
             // // CALCOLO IL PREZZO es. € 0.21 per km e impongo massimo due decimali
             let prezzoStandard = parseFloat(tariffa * chilometri).toFixed(2);
+            // DICHIARO VARIABILI ELEMENTI HTML 
+            let costoBigliettoEl = document.getElementById('costo-biglietto');
+            let tipoOffertaEl = document.getElementById('offerta');
             // SE PASSEGGERO E' MINORENNE APPLICO SCONTO DEL 20% con massimo due decimali
             if (etaPasseggero == 'ticket-minorenni') {
                 let scontoMinorenni = parseFloat(prezzoStandard - ((prezzoStandard * 20) / 100)).toFixed(2);
-                outputMsg = 'Il prezzo riservato ai minorenni è di € ' + scontoMinorenni;
+                // outputMsg = 'Il prezzo riservato ai minorenni è di € ' + scontoMinorenni;
+                costoBigliettoEl.innerHTML = '€ ' + scontoMinorenni;
+                tipoOffertaEl.innerHTML = "Biglietto Young"
             }
             // SE IL PASSEGGERO E' OVER 65 APPLICO SCONTO DEL 40% con massimo due decimali
             else if (etaPasseggero == 'ticket-senior') {
                 let scontoSenior = parseFloat(prezzoStandard - ((prezzoStandard * 40) / 100)).toFixed(2);
-                outputMsg = 'Il prezzo riservato agli over 65 è di € ' + scontoSenior;
+                costoBigliettoEl.innerHTML = '€ ' + scontoSenior;
+                tipoOffertaEl.innerHTML = "Biglietto Senior"
+                // outputMsg = 'Il prezzo riservato agli over 65 è di € ' + scontoSenior;
             }
             else {
-                outputMsg = 'Il prezzo standard è di € ' + prezzoStandard;
+                costoBigliettoEl.innerHTML = '€ ' + prezzoStandard;
+                tipoOffertaEl.innerHTML = "Tariffa Ordinaria"
             }
-            document.getElementById('risultato').innerHTML = `Ciao ${nomeUtente}, ${outputMsg}`;
+            document.getElementById('passeggero').innerHTML = nomeUtente;
+            document.getElementById('carrozza').innerHTML = Math.floor(Math.random() * 20) + 1;
+            document.getElementById('numero-biglietto').innerHTML = '#' + Math.floor(Math.random() * 10000 + 1000);
         }
     }
 )
